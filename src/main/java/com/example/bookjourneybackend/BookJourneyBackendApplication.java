413df45c41678ad9c5ee483396bc56615d721900
janghyunjun <buzz03312@naver.com>
2024-12-31 00:38:53 +0900

    Initial commit

---
+++ src/main/java/com/example/bookjourneybackend/BookJourneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookjourneybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJourneyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookJourneyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookjourneybackend/BookJourneyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookjourneybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJourneyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
