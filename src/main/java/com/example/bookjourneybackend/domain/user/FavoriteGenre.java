90b1fee5637c4573e1814ae489127f2d7a8c3f34
janghyunjun <buzz03312@naver.com>
2025-01-16 22:11:18 +0900

    [feat]: FavoriteGenre 엔티티 추가 (#8)

--- src/main/java/com/example/bookjourneybackend/domain/user/FavoriteGenre.java
+++ src/main/java/com/example/bookjourneybackend/domain/user/FavoriteGenre.java
@@ -1,4 +1,32 @@
 package com.example.bookjourneybackend.domain.user;
 
-public class FavoriteGenre {
+import com.example.bookjourneybackend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class FavoriteGenre extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "favorite_genre_id")
+    private Long favoriteGenreId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //todo Genre ManyToOne 추가
+    @Column(name = "genre_id", nullable = false)
+    private Long genreId;
+
+    @Builder
+    public FavoriteGenre(Long favoriteGenreId, User user, Long genreId) {
+        this.favoriteGenreId = favoriteGenreId;
+        this.user = user;
+        this.genreId = genreId;
+    }
 }
