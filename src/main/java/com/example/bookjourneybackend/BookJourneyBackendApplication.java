263d458a5abf518256c59a8c5f573085d6a0f268
janghyunjun <buzz03312@naver.com>
2025-01-16 22:10:30 +0900

    [chore]: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/example/bookjourneybackend/BookJourneyBackendApplication.java
+++ src/main/java/com/example/bookjourneybackend/BookJourneyBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.bookjourneybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookJourneyBackendApplication {
 
     public static void main(String[] args) {
