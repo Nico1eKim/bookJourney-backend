dfab322ade8be343e824bd35d5efad3de832c9c1
hd0rable <kanghuijin12@gmail.com>
2025-01-19 01:48:39 +0900

    [feat]: RedisConfig,securityConfig,PasswordEncoderConfig 작성 (#23)

---
+++ src/main/java/com/example/bookjourneybackend/global/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.example.bookjourneybackend.global.config;
+
+import com.example.bookjourneybackend.global.util.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
+            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html",
+            "/auth/login","/users/signup","/users/emails/verification-requests","/users/emails/verifications",
+            "/users/nickname","/h2-console/**"
+    };
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //CSRF, CORS
+        http.csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                                .anyRequest().authenticated()
+
+                )
+                //세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                //JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headers ->
+                        headers
+                                .frameOptions().sameOrigin() // H2 콘솔을 허용하도록 프레임 옵션을 sameOrigin으로 설정
+                                .contentSecurityPolicy("frame-ancestors 'self'") // X-Frame-Options 대체
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 허용할 도메인 설정(프엔 서버주소,배포 서버)
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE","PUT"));  // 허용할 HTTP 메서드 설정
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type","Refresh-Token"));  // 허용할 헤더 설정
+        configuration.setAllowCredentials(true);  // 자격 증명 허용 설정
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+
+}
