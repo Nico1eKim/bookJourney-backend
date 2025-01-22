71aaed2d163075c5ec27b7ad8611d18aeadd09f7
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-22 20:39:00 +0900

    [feat]: 같이읽기 방 상세보기 controller 생성 (#33)

--- src/main/java/com/example/bookjourneybackend/domain/readTogether/controller/ReadTogetherController.java
+++ src/main/java/com/example/bookjourneybackend/domain/readTogether/controller/ReadTogetherController.java
@@ -1,4 +1,26 @@
 package com.example.bookjourneybackend.domain.readTogether.controller;
 
+import com.example.bookjourneybackend.domain.readTogether.dto.response.GetRoomInfoResponse;
+import com.example.bookjourneybackend.domain.readTogether.service.ReadTogetherService;
+import com.example.bookjourneybackend.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rooms-together")
 public class ReadTogetherController {
+
+    private final ReadTogetherService readTogetherService;
+
+    @GetMapping("/info/{roomId}")
+    public BaseResponse<GetRoomInfoResponse> getReadTogetherInfo(@PathVariable Long roomId) {
+
+        return BaseResponse.ok(readTogetherService.getRoomInfo(roomId));
+    }
 }
