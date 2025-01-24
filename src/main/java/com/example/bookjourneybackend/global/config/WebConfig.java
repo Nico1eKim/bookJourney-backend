3b16f2284672baabec10f24f53139e7e2029a6e6
janghyunjun <buzz03312@naver.com>
2025-01-25 02:22:49 +0900

    [chore]: LoginUserResolver 설정 및 적용 (#40)

--- src/main/java/com/example/bookjourneybackend/global/config/WebConfig.java
+++ src/main/java/com/example/bookjourneybackend/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.bookjourneybackend.global.config;
 
 import com.example.bookjourneybackend.global.resolver.GetBookListRequestArgumentResolver;
+import com.example.bookjourneybackend.global.resolver.LoginUserResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,14 +12,17 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final GetBookListRequestArgumentResolver getBookListRequestArgumentResolver;
+    private final LoginUserResolver loginUserResolver;
 
-    public WebConfig(GetBookListRequestArgumentResolver getBookListRequestArgumentResolver) {
+    public WebConfig(GetBookListRequestArgumentResolver getBookListRequestArgumentResolver, LoginUserResolver loginUserResolver) {
         this.getBookListRequestArgumentResolver = getBookListRequestArgumentResolver;
+        this.loginUserResolver = loginUserResolver;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         // Argument Resolver 등록
         resolvers.add(getBookListRequestArgumentResolver);
+        resolvers.add(loginUserResolver);
     }
 }
