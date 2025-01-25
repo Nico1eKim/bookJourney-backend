b91e012c2aeac80a3c5e3ab08a6744bddb955904
janghyunjun <buzz03312@naver.com>
2025-01-26 03:41:59 +0900

    [chore]: 혼자읽기를 위해 dto 유효성 검증 완화(#43)

--- src/main/java/com/example/bookjourneybackend/domain/room/dto/request/PostRoomCreateRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/dto/request/PostRoomCreateRequest.java
@@ -13,22 +13,18 @@ public class PostRoomCreateRequest {
 
     private boolean isPublic;
 
-    @NotBlank(message = "방 제목을 입력해주세요.")
     @Size(max = 20, message = "방 제목은 20자 이내입니다.")
     private String roomName;
 
-    @NotNull(message = "시작일자를 작성해주세요.")
     private String progressStartDate;
 
-    @NotNull(message = "종료일자를 작성해주세요.")
     private String progressEndDate;
 
     @Min(value = 1, message = "모집 최소 인원은 1명입니다.")
     @Max(value = 50, message = "모집 최대 인원은 50명입니다.")
     private int recruitCount;
 
-    @Pattern(regexp = "\\d{4}", message = "비밀번호는 4개의 숫자로 이루어져야 합니다.")
-    private String password;
+    private Integer password;
 
     @NotBlank(message = "ISBN cannot be blank.")
     @Pattern(regexp = "\\d{10,13}", message = "ISBN은 10이나 13자리로 이루어집니다.")
