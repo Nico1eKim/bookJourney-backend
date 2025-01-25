c9d69a5186506cdfe8fb0f3579dbf11407b57530
janghyunjun <buzz03312@naver.com>
2025-01-26 05:11:41 +0900

    [feat]: BookRepository에서 Rooms의 size로 정렬하는 쿼리문 작성(#43)

--- src/main/java/com/example/bookjourneybackend/domain/book/domain/repository/BookRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/book/domain/repository/BookRepository.java
@@ -2,12 +2,18 @@ package com.example.bookjourneybackend.domain.book.domain.repository;
 
 import com.example.bookjourneybackend.domain.book.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByIsbn(String isbn);
+
+    @Query("SELECT b FROM Book b LEFT JOIN b.rooms r GROUP BY b ORDER BY COUNT(r) DESC")
+    List<Book> findBookWithMostRooms();
+
 }
