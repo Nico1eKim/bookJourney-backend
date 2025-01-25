f0ff2d0f1f7ae3f6a774d29c7d3612430600a104
janghyunjun <buzz03312@naver.com>
2025-01-26 00:28:07 +0900

    [fix]: conflict 해결후 rebase (#43)

--- src/main/java/com/example/bookjourneybackend/domain/room/controller/RoomController.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/controller/RoomController.java
@@ -1,15 +1,15 @@
 package com.example.bookjourneybackend.domain.room.controller;
 
+import com.example.bookjourneybackend.domain.room.dto.request.PostRoomCreateRequest;
 import com.example.bookjourneybackend.domain.room.dto.response.GetRoomDetailResponse;
 import com.example.bookjourneybackend.domain.room.dto.response.GetRoomInfoResponse;
+import com.example.bookjourneybackend.domain.room.dto.response.PostRoomCreateResponse;
 import com.example.bookjourneybackend.domain.room.service.RoomService;
 import com.example.bookjourneybackend.global.response.BaseResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -29,5 +29,10 @@ public class RoomController {
     public BaseResponse<GetRoomInfoResponse> getRoomInfo(@PathVariable("roomId") Long roomId) {
 
         return BaseResponse.ok(roomService.showRoomInfo(roomId));
+    }
+
+    @PostMapping
+    public BaseResponse<PostRoomCreateResponse> createRoom(@RequestBody @Valid final PostRoomCreateRequest postRoomCreateRequest) {
+        return BaseResponse.ok(roomService.createRoom(postRoomCreateRequest));
     }
 }
--- src/main/java/com/example/bookjourneybackend/domain/room/service/RoomService.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/service/RoomService.java
@@ -1,13 +1,19 @@
 package com.example.bookjourneybackend.domain.room.service;
 
+import com.example.bookjourneybackend.domain.book.domain.Book;
+import com.example.bookjourneybackend.domain.book.domain.repository.BookRepository;
 import com.example.bookjourneybackend.domain.room.domain.Room;
 import com.example.bookjourneybackend.domain.room.domain.repository.RoomRepository;
+import com.example.bookjourneybackend.domain.room.dto.request.PostRoomCreateRequest;
 import com.example.bookjourneybackend.domain.room.dto.response.GetRoomDetailResponse;
 import com.example.bookjourneybackend.domain.room.dto.response.GetRoomInfoResponse;
+import com.example.bookjourneybackend.domain.room.dto.response.PostRoomCreateResponse;
 import com.example.bookjourneybackend.domain.room.dto.response.RoomMemberInfo;
 import com.example.bookjourneybackend.domain.user.domain.User;
 import com.example.bookjourneybackend.domain.user.domain.UserImage;
 import com.example.bookjourneybackend.global.exception.GlobalException;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +31,7 @@ import static com.example.bookjourneybackend.global.response.status.BaseExceptio
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final BookRepository bookRepository;
 
     public GetRoomDetailResponse showRoomDetails(Long roomId) {
         Room room = findRoomById(roomId);
@@ -38,7 +45,7 @@ public class RoomService {
                 room.isPublic(),
                 calculateLastActivityTime(room.getLastActivityTime()),
                 room.getRoomPercentage().intValue(),
-                formatDate(room.getProgressStartDate()),
+                formatDate(room.getStartDate()),
                 formatDate(room.getProgressEndDate()),
                 recruitDday,
                 formatDate(recruitEndDate),
@@ -100,5 +107,45 @@ public class RoomService {
             return "D+" + Math.abs(days);
         }
         return "D-" + days;
+    }
+
+    /**
+     * 1. Book 테이블에 존재하면, 그대로 매핑
+     * 2. Book 테이블에 존재하지 않으면, 알라딘 api를 통해 book 정보를 가져와서 repository에 저장후 매핑
+     * @param postRoomCreateRequest
+     * @return
+     */
+    public PostRoomCreateResponse createRoom(PostRoomCreateRequest postRoomCreateRequest) {
+        Book book = bookRepository.findByIsbn(postRoomCreateRequest.getIsbn())
+                .orElseGet(() -> saveBookFromAladinApi(postRoomCreateRequest.getIsbn()));
+
+        return PostRoomCreateResponse.of(Room.builder()
+                .roomName(postRoomCreateRequest.getRoomName())
+                .book(book)
+                .isPublic(postRoomCreateRequest.isPublic())
+                .password(postRoomCreateRequest.getPassword())
+                .startDate(postRoomCreateRequest.getProgressStartDate())    //방의 생성기간 = 방의 시작 기간 = 방의 모집 시작 기간
+                .progressEndDate(postRoomCreateRequest.getProgressEndDate())
+                .recruitEndDate(calculateRecruitEndDate(postRoomCreateRequest)) //방의 모집종료 기간 = {(방의 종료기간 - 방의 시작기간)/2} + 방의 시작기간
+                .recruitCount(postRoomCreateRequest.getRecruitCount())
+                .roomPercentage(0.0)
+                .recordCount(0)
+                .build());
+    }
+
+    private Book saveBookFromAladinApi(@NotBlank(message = "ISBN cannot be blank.") @Pattern(regexp = "\\d{10,13}", message = "ISBN은 10이나 13자리로 이루어집니다.") String isbn) {
+        return null;
+    }
+
+    /**
+     * //방의 모집종료 기간 = {(방의 종료기간 - 방의 시작기간)/2} + 방의 시작기간
+     * @param postRoomCreateRequest
+     * @return
+     */
+    private LocalDateTime calculateRecruitEndDate(PostRoomCreateRequest postRoomCreateRequest) {
+        long totalDays = ChronoUnit.DAYS.between(postRoomCreateRequest.getProgressStartDate(), postRoomCreateRequest.getProgressEndDate());
+        long halfDays = Math.round(totalDays / 2.0);
+
+        return postRoomCreateRequest.getProgressStartDate().plusDays(halfDays);
     }
 }
