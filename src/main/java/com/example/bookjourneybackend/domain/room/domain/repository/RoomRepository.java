43e393be8f4aff60b81c73972bf677971ab6f2ee
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-26 03:48:38 +0900

    [feat]: 방 검색 repository 작성 (#32)

--- src/main/java/com/example/bookjourneybackend/domain/room/domain/repository/RoomRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/domain/repository/RoomRepository.java
@@ -1,9 +1,34 @@
 package com.example.bookjourneybackend.domain.room.domain.repository;
 
+import com.example.bookjourneybackend.domain.book.domain.GenreType;
 import com.example.bookjourneybackend.domain.room.domain.Room;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("SELECT r FROM Room r " +
+            "WHERE (:searchTerm IS NULL OR r.roomName LIKE CONCAT('%', :searchTerm, '%') " +
+            "OR r.book.bookTitle LIKE CONCAT('%', :searchTerm, '%')) " +
+            "AND (:genre IS NULL OR r.book.genre = :genre) " +
+            "AND (:recruitStartDate IS NULL OR r.recruitStartDate >= :recruitStartDate) " +
+            "AND (:recruitEndDate IS NULL OR r.recruitEndDate <= :recruitEndDate) " +
+            "AND (:roomStartDate IS NULL OR r.progressStartDate >= :roomStartDate) " +
+            "AND (:roomEndDate IS NULL OR r.progressEndDate <= :roomEndDate) " +
+            "AND (:recordCount IS NULL OR SIZE(r.records) >= :recordCount) " +
+            "ORDER BY r.roomId DESC")
+    Slice<Room> findRoomsByFilters(@Param("searchTerm") String searchTerm,
+                                   @Param("genre") GenreType genre,
+                                   @Param("recruitStartDate") LocalDate recruitStartDate,
+                                   @Param("recruitEndDate") LocalDate recruitEndDate,
+                                   @Param("roomStartDate") LocalDate roomStartDate,
+                                   @Param("roomEndDate") LocalDate roomEndDate,
+                                   @Param("recordCount") Integer recordCount,
+                                   Pageable pageable);
 }
