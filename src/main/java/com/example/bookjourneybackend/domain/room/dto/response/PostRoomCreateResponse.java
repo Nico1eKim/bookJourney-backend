1d7e3674ede899ea51f9018e048e874751f367a1
janghyunjun <buzz03312@naver.com>
2025-01-26 03:38:55 +0900

    [refactor]: PostRoomCreateResponse에 NoArgsConstructor 추가 (#43)

--- src/main/java/com/example/bookjourneybackend/domain/room/dto/response/PostRoomCreateResponse.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/dto/response/PostRoomCreateResponse.java
@@ -3,11 +3,16 @@ package com.example.bookjourneybackend.domain.room.dto.response;
 import com.example.bookjourneybackend.domain.room.domain.Room;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class PostRoomCreateResponse {
     private Long roomId;
+
+    public PostRoomCreateResponse(Long roomId) {
+        this.roomId = roomId;
+    }
 
     public static PostRoomCreateResponse of(Room room) {
         return new PostRoomCreateResponse(room.getRoomId());
