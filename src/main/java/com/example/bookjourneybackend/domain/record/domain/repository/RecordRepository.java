1c53c95e2d273654a74c8668a0a77c07033cd3c6
janghyunjun <buzz03312@naver.com>
2025-01-26 21:58:46 +0900

    [chore]: RecordRepository 생성 (#57)

--- src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
@@ -1,4 +1,9 @@
 package com.example.bookjourneybackend.domain.record.domain.repository;
 
-public interface RecordRepository {
+import com.example.bookjourneybackend.domain.record.domain.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
 }
