578b7f39f9322274ba43649bcd0c6053c302c7a2
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-26 22:10:04 +0900

    [feat]: 기록 생성 controller 생성 (#55)

--- src/main/java/com/example/bookjourneybackend/domain/record/controller/RecordController.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/controller/RecordController.java
@@ -1,4 +1,29 @@
 package com.example.bookjourneybackend.domain.record.controller;
 
+import com.example.bookjourneybackend.domain.record.dto.request.PostRecordRequest;
+import com.example.bookjourneybackend.domain.record.dto.response.PostRecordResponse;
+import com.example.bookjourneybackend.domain.record.service.RecordService;
+import com.example.bookjourneybackend.global.annotation.LoginUserId;
+import com.example.bookjourneybackend.global.response.BaseResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/records")
 public class RecordController {
+
+    private final RecordService recordService;
+
+    @PostMapping("/{roomId}")
+    public BaseResponse<PostRecordResponse> createRecord(
+            @PathVariable Long roomId,
+            @RequestBody @Valid PostRecordRequest postRecordRequest,
+            @LoginUserId final Long userId
+    ) {
+        return BaseResponse.ok(recordService.createRecord(postRecordRequest, roomId, userId));
+    }
 }
