d94ae081d0d4e9caf7431135559db2f777b5536f
janghyunjun <buzz03312@naver.com>
2025-01-26 21:58:40 +0900

    [chore]: RecentSearchRepository 생성 (#57)

--- src/main/java/com/example/bookjourneybackend/domain/recentSearch/domain/repository/RecentSearchRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/recentSearch/domain/repository/RecentSearchRepository.java
@@ -1,4 +1,9 @@
 package com.example.bookjourneybackend.domain.recentSearch.domain.repository;
 
-public interface RecentSearchRepository {
+import com.example.bookjourneybackend.domain.recentSearch.domain.RecentSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecentSearchRepository extends JpaRepository<RecentSearch, Long> {
 }
