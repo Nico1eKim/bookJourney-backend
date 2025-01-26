3f7e4dccff4dfcac3a4375ce67d3cf1c690cd4f0
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-26 22:09:52 +0900

    [feat]: 기록 생성 service 로직 작성 (#55)

--- src/main/java/com/example/bookjourneybackend/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/example/bookjourneybackend/global/response/status/BaseExceptionResponseStatus.java
@@ -52,7 +52,16 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
      */
     CANNOT_FIND_ROOM(8001 ,BAD_REQUEST, "방을 찾을 수 없습니다."),
     INVALID_ROOM_TYPE(8002, BAD_REQUEST, "알맞은 방 타입을 찾을 수 없습니다."),
-    INVALID_SEARCH_TYPE(8003, BAD_REQUEST, "알맞은 검색 조건을 찾을 수 없습니다.");
+    INVALID_SEARCH_TYPE(8003, BAD_REQUEST, "알맞은 검색 조건을 찾을 수 없습니다."),
+
+    /**
+     * 9000 : record 관련
+     */
+    RECORD_NOT_FOUND(9001, BAD_REQUEST, "알맞은 기록 타입을 찾을 수 없습니다."),
+
+    INVALID_RECORD_TYPE(9002, BAD_REQUEST, "알맞은 기록 타입을 찾을 수 없습니다."),
+    INVALID_RECORD_PAGE(9002, BAD_REQUEST, "페이지 번호를 입력해주세요."),
+    INVALID_RECORD_TITLE(9002, BAD_REQUEST, "기록 제목을 입력해주세요.");
 
     private final int code;
     private final HttpStatus status;
