a92be905a3478f3d19c11c483adbc41cfb4a6c7e
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-26 23:05:53 +0900

    [refactor]: 어노테이션 수정 (#55)

--- src/main/java/com/example/bookjourneybackend/domain/record/dto/request/PostRecordRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/dto/request/PostRecordRequest.java
@@ -1,6 +1,7 @@
 package com.example.bookjourneybackend.domain.record.dto.request;
 
 import com.example.bookjourneybackend.domain.record.domain.RecordType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PostRecordRequest {
 
-    @NotNull
+    @NotNull(message = "기록의 종류는 필수 입력값입니다.")
     private RecordType recordType;
 
     private String recordTitle; // 전체 기록일 때 필수
     private Integer recordPage; // 페이지 기록일 때 필수
 
-    @NotNull
+    @NotBlank(message = "기록 내용은 필수 입력값입니다.")
     private String content;
 
 
