ccfc299c87d10c393ef099412a9ee2dc1b241545
hd0rable <kanghuijin12@gmail.com>
2025-01-27 05:03:32 +0900

    [refactor]:토큰 재발급 dto 유효성 검증(#45)

--- src/main/java/com/example/bookjourneybackend/domain/auth/domain/dto/request/PostAuthAccessTokenReissueRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/auth/domain/dto/request/PostAuthAccessTokenReissueRequest.java
@@ -1,5 +1,6 @@
 package com.example.bookjourneybackend.domain.auth.domain.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class PostAuthAccessTokenReissueRequest {
 
     //엑세스 토큰 재발급 Request
+    @NotNull(message = "리프레쉬 토큰 입력은 필수입니다")
     private String refreshToken;
 
 }
