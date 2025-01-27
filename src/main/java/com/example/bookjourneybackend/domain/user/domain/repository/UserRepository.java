5d3b473c4c0b162ccfc6576d8ec28aefd9cb835d
hd0rable <kanghuijin12@gmail.com>
2025-01-27 21:23:46 +0900

    [feat]: UserRepository 메서드 작성 (#45)

--- src/main/java/com/example/bookjourneybackend/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/user/domain/repository/UserRepository.java
@@ -3,15 +3,13 @@ package com.example.bookjourneybackend.domain.user.domain.repository;
 import com.example.bookjourneybackend.domain.user.domain.User;
 import com.example.bookjourneybackend.global.entity.EntityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailAndStatus(String email, EntityStatus status);
+    Optional<User> findByUserIdAndStatus(Long userId, EntityStatus status);
 
 
 }
