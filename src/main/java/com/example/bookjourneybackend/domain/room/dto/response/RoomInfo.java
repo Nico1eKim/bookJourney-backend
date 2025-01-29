f2aa5408ea0d4390146a70292a637b23877a5fd9
janghyunjun <buzz03312@naver.com>
2025-01-29 12:19:34 +0900

    [refactor]: RoomInfo, RoomMemberInfo 객체 Builder 패턴 적용 (#68)

--- src/main/java/com/example/bookjourneybackend/domain/room/dto/response/RoomInfo.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/dto/response/RoomInfo.java
@@ -1,9 +1,11 @@
 package com.example.bookjourneybackend.domain.room.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class RoomInfo {
     private Long roomId;
