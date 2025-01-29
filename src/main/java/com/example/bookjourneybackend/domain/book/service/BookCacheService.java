ab503a689354251e72afacb2eeb944195cd79600
hd0rable <kanghuijin12@gmail.com>
2025-01-30 00:51:06 +0900

    [refactor]: 알라딘 api관련 함수 재이용에 따른 함수 리팩토링 (#42)

--- src/main/java/com/example/bookjourneybackend/domain/book/service/BookCacheService.java
+++ src/main/java/com/example/bookjourneybackend/domain/book/service/BookCacheService.java
@@ -74,7 +74,7 @@ public class BookCacheService {
         String currentResponse = aladinApiUtil.requestBookInfoFromAladinApi(requestUrl);
         log.info("알라딘 API 응답 Body: {}", currentResponse);
 
-        Book book = aladinApiUtil.parseAladinApiResponseToBook(currentResponse);
+        Book book = aladinApiUtil.parseAladinApiResponseToBook(currentResponse,false);
 
         return GetBookInfoResponse.of(
                 book.getGenre().getGenreType(),
