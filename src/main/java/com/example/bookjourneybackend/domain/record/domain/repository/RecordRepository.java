b44ceca43ddcf6b02bde7d29ae861593b85c76a1
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-01-29 23:58:04 +0900

    [feat]: 전체 기록 조회 repository 생성 (#63)

--- src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
@@ -1,9 +1,22 @@
 package com.example.bookjourneybackend.domain.record.domain.repository;
 
+import com.example.bookjourneybackend.domain.record.domain.EntireRecordSortType;
 import com.example.bookjourneybackend.domain.record.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+
+    @Query("SELECT r FROM Record r WHERE r.room.roomId = :roomId ORDER BY r.createdAt DESC")
+    List<Record> findRecordsOrderByLatest(@Param("roomId") Long roomId, @Param("sortType") EntireRecordSortType sortType);
+
+    @Query("SELECT r FROM Record r WHERE r.room.roomId = :roomId ORDER BY SIZE(r.comments) DESC")
+    List<Record> findRecordsOrderByMostComments(@Param("roomId") Long roomId, @Param("sortType") EntireRecordSortType sortType);
+
+
 }
