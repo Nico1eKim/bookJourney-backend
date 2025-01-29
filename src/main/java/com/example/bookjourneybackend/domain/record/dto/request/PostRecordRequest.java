5947274aed720fc59f644a5fb608a96f37f77b46
janghyunjun <buzz03312@naver.com>
2025-01-29 14:19:58 +0900

    [refactor]: 기록 작성 리팩토링 (#68)

--- src/main/java/com/example/bookjourneybackend/domain/record/dto/request/PostRecordRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/dto/request/PostRecordRequest.java
@@ -11,13 +11,12 @@ import lombok.NoArgsConstructor;
 public class PostRecordRequest {
 
     @NotNull(message = "기록의 종류는 필수 입력값입니다.")
-    private RecordType recordType;
+    private String recordType;
 
     private String recordTitle; // 전체 기록일 때 필수
     private Integer recordPage; // 페이지 기록일 때 필수
 
     @NotBlank(message = "기록 내용은 필수 입력값입니다.")
     private String content;
-
 
 }
