c1a2bab395ea4d21c010626f2e9ccacee2af20f9
hd0rable <kanghuijin12@gmail.com>
2025-01-30 00:53:21 +0900

    [fix]: 베스트셀러 북 초기 엔티티 수정 (#42)

--- src/main/java/com/example/bookjourneybackend/global/config/initData/BookInitializer.java
+++ src/main/java/com/example/bookjourneybackend/global/config/initData/BookInitializer.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -15,20 +17,48 @@ public class BookInitializer {
     private final BookRepository bookRepository;
 
     public void initializeBooks() {
-        for (int i = 1; i <= 100; i++) {
-            Book book = Book.builder()
-                    .genre(GenreType.NOVEL_POETRY_DRAMA)
-                    .bookTitle("Book Title " + i)
-                    .publisher("Publisher " + i)
+
+        GenreType[] genres = GenreType.values();
+        List<GenreType> validGenres = Arrays.stream(genres)
+                .filter(genre -> genre != GenreType.UNKNOWN) // UNKNOWN 장르 제외
+                .toList();
+
+        int index = 1;
+
+        for (GenreType genre : validGenres) {
+            // bestSeller = true 책 생성
+            Book bestSellerBook = Book.builder()
+                    .genre(genre)
+                    .bookTitle("Best Seller Book " + index)
+                    .publisher("Publisher " + index)
                     .publishedDate(LocalDate.of(2020, 1, 1))
-                    .isbn("123456789012" + i % 10)
-                    .pageCount(200 + i)
-                    .description("Description for Book " + i)
-                    .authorName("Author " + i)
-                    .imageUrl("http://example.com/image" + i)
-                    .bestSeller(i % 2 == 0)
+                    .isbn("123456789012" + index % 10)
+                    .pageCount(200 + index)
+                    .description("Description for Best Seller Book " + index)
+                    .authorName("Author " + index)
+                    .imageUrl("http://example.com/image" + index)
+                    .bestSeller(true)
                     .build();
-            bookRepository.save(book);
+            bookRepository.save(bestSellerBook);
+
+            index++;
+
+            // bestSeller = false 책 생성
+            Book normalBook = Book.builder()
+                    .genre(genre)
+                    .bookTitle("Normal Book " + index)
+                    .publisher("Publisher " + index)
+                    .publishedDate(LocalDate.of(2020, 1, 1))
+                    .isbn("123456789012" + index % 10)
+                    .pageCount(200 + index)
+                    .description("Description for Normal Book " + index)
+                    .authorName("Author " + index)
+                    .imageUrl("http://example.com/image" + index)
+                    .bestSeller(false)
+                    .build();
+            bookRepository.save(normalBook);
+
+            index++;
         }
     }
 }
