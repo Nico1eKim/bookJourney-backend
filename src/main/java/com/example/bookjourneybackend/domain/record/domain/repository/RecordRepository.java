897d0165a9838db42a618a3f7fbeab4f9c9ef7d2
janghyunjun <buzz03312@naver.com>
2025-01-31 03:42:47 +0900

    [fix]: confict 해결 (#80)

--- src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordRepository.java
@@ -2,6 +2,8 @@ package com.example.bookjourneybackend.domain.record.domain.repository;
 
 import com.example.bookjourneybackend.domain.record.domain.RecordSortType;
 import com.example.bookjourneybackend.domain.record.domain.Record;
+import com.example.bookjourneybackend.domain.room.domain.Room;
+import com.example.bookjourneybackend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,4 +23,6 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
     @Query("SELECT r FROM Record r WHERE r.room.roomId = :roomId AND r.status = 'ACTIVE' ORDER BY r.recordPage ASC")
     Optional<List<Record>> findRecordsOrderByPage(@Param("roomId") Long roomId, @Param("sortType") RecordSortType sortType);
+
+    void deleteAllByRoomAndUser(Room room, User user);
 }
