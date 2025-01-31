a7ed35d2a8ff44230b72298621f2be1050987db1
hd0rable <kanghuijin12@gmail.com>
2025-01-31 10:24:16 +0900

    [refactor]: 더미데이터 비밀번호 인코딩해서 들어가도록 리펙(#88)

--- src/main/java/com/example/bookjourneybackend/global/config/initData/UserInitializer.java
+++ src/main/java/com/example/bookjourneybackend/global/config/initData/UserInitializer.java
@@ -3,6 +3,7 @@ package com.example.bookjourneybackend.global.config.initData;
 import com.example.bookjourneybackend.domain.user.domain.User;
 import com.example.bookjourneybackend.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,12 +11,13 @@ import org.springframework.stereotype.Component;
 public class UserInitializer {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void initializeUsers() {
         for (int i = 1; i <= 50; i++) {
             User user = User.builder()
                     .email("user" + i + "@example.com")
-                    .password("password" + i)
+                    .password(passwordEncoder.encode("password" + i))
                     .nickname("User" + i)
                     .build();
             userRepository.save(user);
