fa440ae46e99b451adba50d6d34cc23c06b0a71c
hd0rable <kanghuijin12@gmail.com>
2025-01-31 22:52:53 +0900

    [refactor]: 검증 dto 하나로 합쳐서 작성(#83)

---
+++ src/main/java/com/example/bookjourneybackend/domain/user/domain/dto/response/PostUsersValidationResponse.java
@@ -0,0 +1,19 @@
+package com.example.bookjourneybackend.domain.user.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
+public class PostUsersValidationResponse {
+
+    private boolean verified;
+
+    public static PostUsersValidationResponse of(boolean verified) {
+        return new PostUsersValidationResponse(verified);
+    }
+}
