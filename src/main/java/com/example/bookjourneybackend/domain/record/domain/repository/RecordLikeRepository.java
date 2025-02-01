25d438c93b9e0a755191e4d62be005e9a70a4040
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-01 23:28:18 +0900

    [feat]: 기록 좋아요 repository 작성 (#85)

--- src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordLikeRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/record/domain/repository/RecordLikeRepository.java
@@ -6,9 +6,12 @@ import com.example.bookjourneybackend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecordLikeRepository extends JpaRepository<RecordLike, Long> {
 
     boolean existsByRecordAndUser(Record record, User user);
 
+    void deleteByRecordAndUser(Record record, User user);
 }
