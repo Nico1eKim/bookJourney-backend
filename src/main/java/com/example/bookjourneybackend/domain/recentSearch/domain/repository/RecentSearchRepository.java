03c44b4b1fcc457e56dd66a5bf1fe8595f09644b
hd0rable <kanghuijin12@gmail.com>
2025-02-02 23:39:56 +0900

    [feat]: 최근검색어 조회 RecentSearchRepository 작성 (#16)

--- src/main/java/com/example/bookjourneybackend/domain/recentSearch/domain/repository/RecentSearchRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/recentSearch/domain/repository/RecentSearchRepository.java
@@ -1,9 +1,14 @@
 package com.example.bookjourneybackend.domain.recentSearch.domain.repository;
 
 import com.example.bookjourneybackend.domain.recentSearch.domain.RecentSearch;
+import com.example.bookjourneybackend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RecentSearchRepository extends JpaRepository<RecentSearch, Long> {
+    Optional<List<RecentSearch>> findByUser(User user);
 }
