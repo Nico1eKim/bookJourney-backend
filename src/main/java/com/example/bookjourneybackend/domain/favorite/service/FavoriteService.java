b4fd2371c58ca921a8bf701dd98d81cb7978084b
hd0rable <kanghuijin12@gmail.com>
2025-02-03 16:43:44 +0900

    [feat]: 즐겨찾기 추가 FavoriteService.addFavorite 작성 (#103)

--- src/main/java/com/example/bookjourneybackend/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/example/bookjourneybackend/domain/favorite/service/FavoriteService.java
@@ -1,4 +1,87 @@
 package com.example.bookjourneybackend.domain.favorite.service;
 
+import com.example.bookjourneybackend.domain.book.domain.Book;
+import com.example.bookjourneybackend.domain.book.domain.GenreType;
+import com.example.bookjourneybackend.domain.book.domain.repository.BookRepository;
+import com.example.bookjourneybackend.domain.book.dto.response.GetBookInfoResponse;
+import com.example.bookjourneybackend.domain.book.service.BookCacheService;
+import com.example.bookjourneybackend.domain.favorite.domain.Favorite;
+import com.example.bookjourneybackend.domain.favorite.domain.dto.response.PostFavoriteAddResponse;
+import com.example.bookjourneybackend.domain.favorite.domain.repository.FavoriteRepository;
+import com.example.bookjourneybackend.domain.user.domain.User;
+import com.example.bookjourneybackend.domain.user.domain.repository.UserRepository;
+import com.example.bookjourneybackend.global.exception.GlobalException;
+import com.example.bookjourneybackend.global.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_BOOK;
+import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_USER;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
+    private final BookCacheService bookCacheService;
+    private final DateUtil dateUtil;
+
+    /**
+     * isbn에 해당하는 책을 즐겨찾기에 추가
+     * 1. DB에 존재하면 DB에 존재하는 책을 바로 즐겨찾기 테이블에 추가
+     * 2. 존재하지 않는 다면 캐시저장소에서 정보 찾아와서 DB저장후 즐겨찾기 테이블에 추가
+     * @param isbn,userId
+     * @return PostFavoriteAddResponse
+     */
+    public PostFavoriteAddResponse addFavorite(String isbn, Long userId) {
+        log.info("[FavoriteService.addFavorite]");
+
+        // 사용자 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new GlobalException(CANNOT_FOUND_USER));
+
+
+        //DB에 책 저장확인 없으면 캐시 저장소에서 확인
+        Book findBook = bookRepository.findByIsbn(isbn)
+                .orElseGet(() -> {
+                    //TODO bookCacheService.checkBookInfo retrun 형태 book으로 바뀌면 코드 리펙
+                    GetBookInfoResponse getBookInfoResponse = bookCacheService.checkBookInfo(isbn);
+
+                    if (getBookInfoResponse == null) {
+                        throw new GlobalException(CANNOT_FOUND_BOOK);
+                    }
+                    getBookInfoResponse.setFavorite(true);
+                    Book book = Book.builder()
+                            .isbn(isbn)
+                            .bookTitle(getBookInfoResponse.getBookTitle())
+                            .authorName(getBookInfoResponse.getAuthorName())
+                            .genre(GenreType.parsingGenreType(getBookInfoResponse.getGenre()))
+                            .publisher(getBookInfoResponse.getPublisher())
+                            .publishedDate(dateUtil.parseDateToLocalDateFromPublishedDateString(getBookInfoResponse.getPublishedDate()))
+                            .description(getBookInfoResponse.getDescription())
+                            .imageUrl(getBookInfoResponse.getImageUrl())
+                            .bestSeller(false)
+                            .pageCount(null)
+                            .build();
+
+                    //캐시 저장소에서 찾은 책 DB저장
+                    bookRepository.save(book);
+                    return book;
+                });
+
+        //즐겨찾기 추가
+        favoriteRepository.save(Favorite.builder()
+                .book(findBook)
+                .user(user)
+                .build());
+
+        return PostFavoriteAddResponse.of(findBook.getBookId(),true);
+    }
+
 }
