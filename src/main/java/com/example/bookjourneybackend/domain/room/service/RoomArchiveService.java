c9c68b073891bdd16a5d586fb4588d31699ad539
janghyunjun <buzz03312@naver.com>
2025-02-03 19:45:37 +0900

    [feat]: 독서기록장(다 읽었어요) api 구현 (#104)

--- src/main/java/com/example/bookjourneybackend/domain/room/service/RoomArchiveService.java
+++ src/main/java/com/example/bookjourneybackend/domain/room/service/RoomArchiveService.java
@@ -7,6 +7,7 @@ import com.example.bookjourneybackend.domain.room.dto.response.GetRoomArchiveRes
 import com.example.bookjourneybackend.domain.room.dto.response.RecordInfo;
 import com.example.bookjourneybackend.domain.userRoom.domain.UserRoom;
 import com.example.bookjourneybackend.domain.userRoom.domain.repository.UserRoomRepository;
+import com.example.bookjourneybackend.global.entity.EntityStatus;
 import com.example.bookjourneybackend.global.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,9 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.example.bookjourneybackend.global.entity.EntityStatus.EXPIRED;
+import static com.example.bookjourneybackend.global.entity.EntityStatus.INACTIVE;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -28,15 +32,13 @@ public class RoomArchiveService {
     private final DateUtil dateUtil;
 
     /**
-     * <독서기록장 다 안읽었어요>
-     * 해당 사용자의 status가 'INACTIVE'인 UserRoom 중에서 queryParam으로 넘어온 날짜에 겹치는 날을 필터링하여 조회
+     * status=INACTIVE => <독서기록장 다 안읽었어요>
+     * status=EXPIRED => <독서기록장 다 읽었어요>
+     * 해당 사용자의 status에 따른 UserRoom 중에서 queryParam으로 넘어온 날짜에 겹치는 날을 필터링하여 조회
      * 정렬은 inActivatedAt 내림차순으로 정렬
      */
-    public GetRoomArchiveResponse viewInCompletedRooms(Long userId, Integer month, Integer year) {
-        log.info("------------------------[RoomArchiveService.viewCompletedRooms]------------------------");
-
-        List<UserRoom> togetherArchiveList;
-        List<UserRoom> aloneArchiveList;
+    public GetRoomArchiveResponse viewArchiveRooms(Long userId, Integer month, Integer year, EntityStatus status) {
+        log.info("------------------------[RoomArchiveService.viewArchiveRooms]------------------------");
 
         if (year == null) {
             // year가 null이면 이번달과 겹치는 모든 방을 조회
@@ -45,19 +47,32 @@ public class RoomArchiveService {
         }
 
         //month가 null일 경우에는 해당 년도의 모든 방을 조회
-        togetherArchiveList = userRoomRepository.findInActiveTogetherRoomsByUserIdAndDate(userId, year, month);
-        aloneArchiveList = userRoomRepository.findInActiveAloneRoomsByUserIdAndDate(userId, year, month);
+        List<UserRoom> togetherArchiveList = userRoomRepository.findInActiveTogetherRoomsByUserIdAndDate(userId, year, month, status);
+        List<UserRoom> aloneArchiveList = userRoomRepository.findInActiveAloneRoomsByUserIdAndDate(userId, year, month, status);
 
-        return new GetRoomArchiveResponse(combineAndParseToRecordInfo(togetherArchiveList, aloneArchiveList));
+        return new GetRoomArchiveResponse(combineAndParseToRecordInfo(togetherArchiveList, aloneArchiveList, status));
     }
 
     //DB에서 찾은 같이읽기와 혼자읽기 방을 InActivatedAt 내림차순으로 정렬하여 RecordInfo의 리스트로 파싱
-    private List<RecordInfo> combineAndParseToRecordInfo(List<UserRoom> togetherArchiveList, List<UserRoom> aloneArchiveList) {
+    private List<RecordInfo> combineAndParseToRecordInfo(List<UserRoom> togetherArchiveList, List<UserRoom> aloneArchiveList, EntityStatus status) {
         List<UserRoom> combinedList = new ArrayList<>();
         combinedList.addAll(togetherArchiveList);
         combinedList.addAll(aloneArchiveList);
 
-        combinedList.sort(Comparator.comparing(UserRoom::getInActivatedAt).reversed());
+        if (status == INACTIVE) {
+            combinedList.sort(Comparator.comparing(UserRoom::getInActivatedAt).reversed());
+        }
+        if(status == EXPIRED) {
+            //혼자읽기책과 같이읽기방을 구분하지 않고, 기간의 마지막일 날짜가 가장 오래 전인 책 및 방이 가장 상단에 위치하도록 정렬
+            //기간의 마지막일이 같으면?
+            //혼자읽기책과 같이읽기 방 중에서는 혼자읽기 책이 위에 배치되고 -> 제외
+            //시작일을 기준으로, 시작일이 더 예전일 때 위에 배치
+            //(혼자읽기 책의 경우, 시작일 = 유저가 혼자읽기 버튼을 누른 시점
+            //같이읽기 방의 경우, 시작일 = 방기간의 시작일)
+            combinedList.sort(Comparator.comparing(UserRoom::getRoom , Comparator.comparing(Room::getProgressEndDate, Comparator.nullsLast(Comparator.reverseOrder())))
+                    .thenComparing(UserRoom::getRoom, Comparator.comparing(Room::getStartDate, Comparator.naturalOrder())));
+        }
+
 
         return combinedList.stream()
                 .map(userRoom -> {
