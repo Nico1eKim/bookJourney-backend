d0340c2a89c34b27e605ba71c2d070ef301ed8e1
hd0rable <kanghuijin12@gmail.com>
2025-02-03 16:44:00 +0900

    [feat]: 즐겨찾기 추가 FavoriteController.addFavorite 작성 (#103)

--- src/main/java/com/example/bookjourneybackend/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/bookjourneybackend/domain/favorite/controller/FavoriteController.java
@@ -1,13 +1,12 @@
 package com.example.bookjourneybackend.domain.favorite.controller;
 
+import com.example.bookjourneybackend.domain.favorite.domain.dto.response.PostFavoriteAddResponse;
 import com.example.bookjourneybackend.domain.favorite.service.FavoriteService;
+import com.example.bookjourneybackend.global.annotation.LoginUserId;
 import com.example.bookjourneybackend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,8 +16,9 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
-    @GetMapping("/{isbn}")
-    public BaseResponse<> favoriteAdd(@PathVariable) {
-
+    @PostMapping("/{isbn}")
+    public BaseResponse<PostFavoriteAddResponse> addFavorite(@PathVariable("isbn") final String isbn, @LoginUserId final Long userId) {
+        log.info("[FavoriteController.addFavorite]");
+        return BaseResponse.ok(favoriteService.addFavorite(isbn,userId));
     }
 }
