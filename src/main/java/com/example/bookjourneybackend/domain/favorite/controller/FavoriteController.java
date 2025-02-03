d94cf4e638ae6ef32be217ff87a4d9924c5cad0e
hd0rable <kanghuijin12@gmail.com>
2025-02-03 13:56:58 +0900

    [feat]: 즐겨찾기 추가 구현 ing.. (#103)

--- src/main/java/com/example/bookjourneybackend/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/bookjourneybackend/domain/favorite/controller/FavoriteController.java
@@ -1,4 +1,24 @@
 package com.example.bookjourneybackend.domain.favorite.controller;
 
+import com.example.bookjourneybackend.domain.favorite.service.FavoriteService;
+import com.example.bookjourneybackend.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/favorites")
+@RequiredArgsConstructor
 public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @GetMapping("/{isbn}")
+    public BaseResponse<> favoriteAdd(@PathVariable) {
+
+    }
 }
