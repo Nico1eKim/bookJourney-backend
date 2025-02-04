0edef251584a26354eba3a700a54e6a55ad7f905
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 02:16:16 +0900

    [feat]: 댓글 조회 service 작성 (#107)

--- src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
@@ -1,4 +1,65 @@
 package com.example.bookjourneybackend.domain.comment.service;
 
+import com.example.bookjourneybackend.domain.comment.domain.Comment;
+import com.example.bookjourneybackend.domain.comment.domain.dto.response.CommentInfo;
+import com.example.bookjourneybackend.domain.comment.domain.dto.response.GetCommentListResponse;
+import com.example.bookjourneybackend.domain.comment.domain.repository.CommentLikeRepository;
+import com.example.bookjourneybackend.domain.comment.domain.repository.CommentRepository;
+import com.example.bookjourneybackend.domain.record.domain.Record;
+import com.example.bookjourneybackend.domain.record.domain.repository.RecordRepository;
+import com.example.bookjourneybackend.domain.user.domain.User;
+import com.example.bookjourneybackend.domain.user.domain.repository.UserRepository;
+import com.example.bookjourneybackend.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_RECORD;
+import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_USER;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
+    private final UserRepository userRepository;
+    private final RecordRepository recordRepository;
+
+    @Transactional(readOnly = true)
+    public GetCommentListResponse showComments(Long recordId, Long userId) {
+        Record record = recordRepository.findById(recordId).orElseThrow(() -> new GlobalException(CANNOT_FOUND_RECORD));
+        User user = userRepository.findById(userId).orElseThrow(() -> new GlobalException(CANNOT_FOUND_USER));
+
+        List<Comment> comments = commentRepository.findByRecord(record);
+
+        List<CommentInfo> commentList = comments.stream()
+                .map(comment -> CommentInfo.from(comment, commentLikeRepository.existsByCommentAndUser(comment, user)))
+                .toList();
+
+        return GetCommentListResponse.of(commentList);
+//        List<Comment> comments = commentRepository.findByRecord_RecordId(recordId);
+//
+//        List<CommentInfo> commentList = comments.stream()
+//                .map(comment -> {
+//                    boolean isLiked = commentLikeRepository.existsByCommentAndUser_UserId(comment, userId);
+//                    return new CommentInfo(
+//                            comment.getCommentId(),
+//                            comment.getRecord().getRecordId(),
+//                            comment.getUser().getUserId(),
+//                            (comment.getUser().getUserImage() != null) ? comment.getUser().getUserImage().toString() : null,
+//                            comment.getUser().getNickname(),
+//                            comment.getContent(),
+//                            comment.getCommentLikes().size(),
+//                            isLiked
+//                    );
+//                })
+//                .toList();
+//
+//        return GetCommentListResponse.of(commentList);
+    }
 }
