27c3b093127d618fd69700bd5760ef9b352061cb
hd0rable <kanghuijin12@gmail.com>
2025-02-05 05:24:16 +0900

    [feat]: 즐겨찾기 선택 삭제 dto 작성 (#117)

--- src/main/java/com/example/bookjourneybackend/domain/favorite/domain/dto/request/DeleteFavoriteSelectedRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/favorite/domain/dto/request/DeleteFavoriteSelectedRequest.java
@@ -1,9 +1,29 @@
 package com.example.bookjourneybackend.domain.favorite.domain.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
 public class DeleteFavoriteSelectedRequest {
+
+    private List<FavoriteInfo> favoriteList;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor(access = PRIVATE)
+    public static class FavoriteInfo {
+
+        private Long favoriteId;
+    }
+
 }
