2c040d6028d883593af5382bf2a228df89cc7fba
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 02:38:05 +0900

    [refactor]: 댓글에 댓글 남긴 시간 추가 (#107)

--- src/main/java/com/example/bookjourneybackend/domain/comment/domain/dto/response/CommentInfo.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/domain/dto/response/CommentInfo.java
@@ -1,9 +1,13 @@
 package com.example.bookjourneybackend.domain.comment.domain.dto.response;
 
 import com.example.bookjourneybackend.domain.comment.domain.Comment;
+import com.example.bookjourneybackend.global.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
@@ -14,6 +18,7 @@ public class CommentInfo {
     private Long userId;
     private String imageUrl;
     private String nickName;
+    private String createdAt;
     private String content;
     private int commentLikeCount;
     private boolean isLike;
@@ -25,6 +30,7 @@ public class CommentInfo {
                 .userId(comment.getUser().getUserId())
                 .imageUrl(comment.getUser().getUserImage() != null ? comment.getUser().getUserImage().toString() : null)
                 .nickName(comment.getUser().getNickname())
+                .createdAt(comment.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .content(comment.getContent())
                 .commentLikeCount(comment.getCommentLikes().size())
                 .isLike(isLiked)
