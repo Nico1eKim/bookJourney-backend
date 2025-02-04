06103435b760a23e05e813fbbde61fb1b4d6a3be
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 02:15:20 +0900

    [feat]: 댓글 조회 controller 작성 (#107)

--- src/main/java/com/example/bookjourneybackend/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/controller/CommentController.java
@@ -1,4 +1,29 @@
 package com.example.bookjourneybackend.domain.comment.controller;
 
+import com.example.bookjourneybackend.domain.comment.domain.dto.response.GetCommentListResponse;
+import com.example.bookjourneybackend.domain.comment.service.CommentService;
+import com.example.bookjourneybackend.global.annotation.LoginUserId;
+import com.example.bookjourneybackend.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/{recordId}")
+    public BaseResponse<GetCommentListResponse> getComments(
+            @PathVariable final Long recordId,
+            @LoginUserId final Long userId
+    ) {
+        return BaseResponse.ok(commentService.showComments(recordId, userId));
+    }
 }
