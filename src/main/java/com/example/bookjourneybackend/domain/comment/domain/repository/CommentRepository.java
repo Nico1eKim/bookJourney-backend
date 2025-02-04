bd94ccaecc95128e085ae8b8c3ecb1692c0d2646
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 02:16:08 +0900

    [feat]: 댓글 조회 repository 작성 (#107)

--- src/main/java/com/example/bookjourneybackend/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/domain/repository/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.example.bookjourneybackend.domain.comment.domain.repository;
 
 import com.example.bookjourneybackend.domain.comment.domain.Comment;
+import com.example.bookjourneybackend.domain.record.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+        List<Comment> findByRecord(Record record);
+//    List<Comment> findByRecord_RecordId(Long recordId);
 }
