b7b93f0d762f2ee9aef3f06f9bafa23503e65db2
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 22:54:50 +0900

    [feat]: 댓글 생성 service 작성 (#108)

--- src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
@@ -1,14 +1,19 @@
 package com.example.bookjourneybackend.domain.comment.service;
 
 import com.example.bookjourneybackend.domain.comment.domain.Comment;
+import com.example.bookjourneybackend.domain.comment.domain.dto.request.PostCommentRequest;
 import com.example.bookjourneybackend.domain.comment.domain.dto.response.CommentInfo;
 import com.example.bookjourneybackend.domain.comment.domain.dto.response.GetCommentListResponse;
+import com.example.bookjourneybackend.domain.comment.domain.dto.response.PostCommentResponse;
 import com.example.bookjourneybackend.domain.comment.domain.repository.CommentLikeRepository;
 import com.example.bookjourneybackend.domain.comment.domain.repository.CommentRepository;
 import com.example.bookjourneybackend.domain.record.domain.Record;
 import com.example.bookjourneybackend.domain.record.domain.repository.RecordRepository;
+import com.example.bookjourneybackend.domain.room.domain.Room;
 import com.example.bookjourneybackend.domain.user.domain.User;
 import com.example.bookjourneybackend.domain.user.domain.repository.UserRepository;
+import com.example.bookjourneybackend.domain.userRoom.domain.UserRoom;
+import com.example.bookjourneybackend.domain.userRoom.domain.repository.UserRoomRepository;
 import com.example.bookjourneybackend.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +22,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_RECORD;
-import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_USER;
+import static com.example.bookjourneybackend.global.entity.EntityStatus.DELETED;
+import static com.example.bookjourneybackend.global.entity.EntityStatus.EXPIRED;
+import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.*;
 
 @Slf4j
 @Service
@@ -29,6 +35,7 @@ public class CommentService {
     private final CommentLikeRepository commentLikeRepository;
     private final UserRepository userRepository;
     private final RecordRepository recordRepository;
+    private final UserRoomRepository userRoomRepository;
 
     @Transactional(readOnly = true)
     public GetCommentListResponse showComments(Long recordId, Long userId) {
@@ -42,5 +49,33 @@ public class CommentService {
                 .toList();
 
         return GetCommentListResponse.of(commentList);
+    }
+
+    @Transactional
+    public PostCommentResponse createComment(Long recordId, Long userId, PostCommentRequest request) {
+        Record record = recordRepository.findById(recordId).orElseThrow(() -> new GlobalException(CANNOT_FOUND_RECORD));
+        User user = userRepository.findById(userId).orElseThrow(() -> new GlobalException(CANNOT_FOUND_USER));
+        Room room = record.getRoom();
+        UserRoom userRoom = userRoomRepository.findUserRoomByRoomAndUser(room, user).orElseThrow(() -> new GlobalException(CANNOT_FOUND_USER_ROOM));
+
+        // 유저가 방에 속해 있지 않거나, 방에서 삭제된 경우 예외 발생
+        if (userRoom.getStatus() == DELETED) {
+            throw new GlobalException(NOT_PARTICIPATING_IN_ROOM);
+        }
+
+        // 방이 EXPIRED 상태이면 댓글을 남길 수 없음
+        if (room.getStatus() == EXPIRED) {
+            throw new GlobalException(CANNOT_WRITE_IN_EXPIRED_ROOM);
+        }
+
+        Comment newComment = Comment.builder()
+                .record(record)
+                .user(user)
+                .content(request.getContent())
+                .build();
+
+        commentRepository.save(newComment);
+
+        return PostCommentResponse.of(newComment.getCommentId());
     }
 }
