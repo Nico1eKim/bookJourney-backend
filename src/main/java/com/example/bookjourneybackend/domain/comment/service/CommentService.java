3ae5d71724e5833ec01aa7dcd8888f97415f9202
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-02-05 22:16:17 +0900

    [refactor]: 댓글 조회 시 기록도 조회하게 service 수정 (#132)

--- src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
+++ src/main/java/com/example/bookjourneybackend/domain/comment/service/CommentService.java
@@ -6,10 +6,13 @@ import com.example.bookjourneybackend.domain.comment.domain.dto.response.GetComm
 import com.example.bookjourneybackend.domain.comment.domain.repository.CommentLikeRepository;
 import com.example.bookjourneybackend.domain.comment.domain.repository.CommentRepository;
 import com.example.bookjourneybackend.domain.record.domain.Record;
+import com.example.bookjourneybackend.domain.record.domain.repository.RecordLikeRepository;
 import com.example.bookjourneybackend.domain.record.domain.repository.RecordRepository;
+import com.example.bookjourneybackend.domain.record.dto.response.RecordInfo;
 import com.example.bookjourneybackend.domain.user.domain.User;
 import com.example.bookjourneybackend.domain.user.domain.repository.UserRepository;
 import com.example.bookjourneybackend.global.exception.GlobalException;
+import com.example.bookjourneybackend.global.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.example.bookjourneybackend.domain.record.domain.RecordType.PAGE;
 import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_RECORD;
 import static com.example.bookjourneybackend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_USER;
 
@@ -29,6 +33,8 @@ public class CommentService {
     private final CommentLikeRepository commentLikeRepository;
     private final UserRepository userRepository;
     private final RecordRepository recordRepository;
+    private final RecordLikeRepository recordLikeRepository;
+    private final DateUtil dateUtil;
 
     @Transactional(readOnly = true)
     public GetCommentListResponse showComments(Long recordId, Long userId) {
@@ -41,6 +47,37 @@ public class CommentService {
                 .map(comment -> CommentInfo.from(comment, commentLikeRepository.existsByCommentAndUser(comment, user)))
                 .toList();
 
-        return GetCommentListResponse.of(commentList);
+        RecordInfo recordInfo;
+        boolean isLiked = recordLikeRepository.existsByRecordAndUser(record, user);
+
+        if (record.getRecordType() == PAGE) {
+            recordInfo = RecordInfo.fromPageRecord(
+                    record.getUser().getUserId(),
+                    record.getRecordId(),
+                    (record.getUser().getUserImage() != null) ? record.getUser().getUserImage().getImageUrl() : null,
+                    record.getUser().getNickname(),
+                    record.getRecordPage(),
+                    dateUtil.formatLocalDateTime(record.getCreatedAt()),
+                    record.getContent(),
+                    record.getComments().size(),
+                    record.getRecordLikes().size(),
+                    isLiked
+            );
+        } else {
+            recordInfo = RecordInfo.fromEntireRecord(
+                    record.getUser().getUserId(),
+                    record.getRecordId(),
+                    (record.getUser().getUserImage() != null) ? record.getUser().getUserImage().getImageUrl() : null,
+                    record.getUser().getNickname(),
+                    record.getRecordTitle(),
+                    dateUtil.formatLocalDateTime(record.getCreatedAt()),
+                    record.getContent(),
+                    record.getComments().size(),
+                    record.getRecordLikes().size(),
+                    isLiked
+            );
+        }
+
+        return GetCommentListResponse.of(commentList, recordInfo);
     }
 }
