fa6e8a1dff5010c6ef1052a0ecd72d76d71effb7
janghyunjun <buzz03312@naver.com>
2025-02-08 00:15:03 +0900

    [chore]: User dto 패키지 이동 (#139)

--- src/main/java/com/example/bookjourneybackend/domain/user/dto/request/PostUsersNicknameValidationRequest.java
+++ src/main/java/com/example/bookjourneybackend/domain/user/dto/request/PostUsersNicknameValidationRequest.java
@@ -1,4 +1,4 @@
-package com.example.bookjourneybackend.domain.user.domain.dto.request;
+package com.example.bookjourneybackend.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
