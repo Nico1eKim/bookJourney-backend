4ef0bcb729a331d6373fde7824ed0f219494d757
janghyunjun <buzz03312@naver.com>
2025-02-09 20:22:21 +0900

    [feat]: 독서달력 조회 핸들러 작성 (#156)

---
+++ src/main/java/com/example/bookjourneybackend/domain/user/controller/MyPageController.java
@@ -0,0 +1,27 @@
+package com.example.bookjourneybackend.domain.user.controller;
+
+import com.example.bookjourneybackend.domain.user.dto.response.GetMyPageCalendarResponse;
+import com.example.bookjourneybackend.domain.user.service.MyPageService;
+import com.example.bookjourneybackend.global.annotation.LoginUserId;
+import com.example.bookjourneybackend.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users/mypage")
+public class MyPageController {
+
+    private final MyPageService myPageService;
+
+    @GetMapping("/calendar")
+    public BaseResponse<GetMyPageCalendarResponse> getMyPageCalendar(
+            @LoginUserId final Long userId,
+            @RequestParam final Integer month,
+            @RequestParam final Integer year) {
+        return BaseResponse.ok(myPageService.showMyPageCalendar(userId, month, year));
+    }
+}
