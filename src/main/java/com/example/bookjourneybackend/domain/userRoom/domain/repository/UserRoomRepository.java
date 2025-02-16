f933db95dbae3953797c3b45f5b5df26b553759e
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-02-16 14:34:04 +0900

    [FIX] 독서기록장 혼자읽기 경우 쿼리 수정

--- src/main/java/com/example/bookjourneybackend/domain/userRoom/domain/repository/UserRoomRepository.java
+++ src/main/java/com/example/bookjourneybackend/domain/userRoom/domain/repository/UserRoomRepository.java
@@ -43,11 +43,11 @@ public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
             "AND r.roomType = 'TOGETHER'")
    List<UserRoom> findInActiveTogetherRoomsByUserIdAndDate(@Param("userId") Long userId, @Param("year") Integer year, @Param("month") Integer month, @Param("status") EntityStatus status);
 
-    @Query("SELECT ur FROM UserRoom ur " +
+     @Query("SELECT ur FROM UserRoom ur " +
             "JOIN FETCH ur.room r " +
             "WHERE ur.user.userId = :userId AND ur.status = :status " +
             "AND ((:month IS NULL OR MONTH(r.startDate) <= :month) AND YEAR(r.startDate) <= :year) " +
-            "AND (r.progressEndDate IS NULL OR (MONTH(r.progressEndDate) >= :month AND YEAR(r.progressEndDate) >= :year)) " +
+            "AND (r.progressEndDate IS NULL OR ((:month IS NULL OR MONTH(r.progressEndDate) >= :month) AND YEAR(r.progressEndDate) >= :year)) " +
             "AND r.roomType = 'ALONE'")
     List<UserRoom> findInActiveAloneRoomsByUserIdAndDate(@Param("userId") Long userId, @Param("year") Integer year, @Param("month") Integer month, @Param("status") EntityStatus status);
 
